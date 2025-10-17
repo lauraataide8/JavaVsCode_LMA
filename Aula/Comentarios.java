438fc4796ce15c9046c9b5e4f9da9c66ac7829a5
Laura Ataide <lauramartinsataide@gmail.com>
2025-10-17 13:57:00 -0300

    Adicionando arquivo OlaMundo

--- Aula/Comentarios.java
+++ Aula/Comentarios.java
@@ -1,4 +1,13 @@
 public class Comentarios {
     // Criando o metodo Main, esse comentario nao aparece para o usuário
-    public static void main()
+    public static void main(String [] args) {
+        /*
+        public refere-se a visibilidade do método.
+        static refere-se a propriedade da classe.
+        void refere-se ao método não retornar nenhum valor.
+        main refere-se ao nome do método de execução de toda a aplicação.
+        String[]args é o vetor de string do java.
+        */
+
+    }
 }
