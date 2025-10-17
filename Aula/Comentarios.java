c6a7421d84b3e937e5bfe4bb909423190652f16e
Laura Ataide <lauramartinsataide@gmail.com>
2025-10-17 13:51:54 -0300

    Primeiro commit

---
+++ Aula/Comentarios.java
@@ -0,0 +1,4 @@
+public class Comentarios {
+    // Criando o metodo Main, esse comentario nao aparece para o usuário
+    public static void main()
+}
---
+++ Vscode/OlaMundo.java
@@ -0,0 +1,13 @@
+public class OlaMundo{
+public static void main(String[] args){
+    exibir();
+    
+}
+public static void exibir (){
+    System.out.println("Olá Mundo!");
+    System.out.println("Como dizia o professor Gustavo Guanabara, Me livrei da maldição");
+}
+
+
+}
+ 
